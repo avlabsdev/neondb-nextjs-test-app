ebc43c37ef97333ece2ab94d28e57a69f889371f
Aaron Varga <hello@avlabs.dev>
2024-11-15 20:02:30 -0500

    country page updates

--- my-app/app/countries/page.tsx
+++ my-app/app/countries/page.tsx
@@ -1,16 +1,23 @@
 import { createClient } from "@/utils/supabase/server";
+import Link from "next/link";
 
 export default async function Countries() {
   const supabase = await createClient();
   const { data: countries } = await supabase.from("countries").select();
 
   return (
-    <div>
+    <div className="flex flex-col gap-8">
       <h1 className="text-2xl font-bold">Countries:</h1>
-      <ul className="mt-8 list-disc">
+      <ul className="list-disc">
         {countries &&
           countries.map((country) => <li key={country.id}>{country.name}</li>)}
       </ul>
+      <Link
+        href="/"
+        className="bg-blue-500 w-fit py-4 px-8 rounded-xl text-white font-semibold"
+      >
+        Back to Home
+      </Link>
     </div>
   );
 }
